a23c3b64cfcce37740c96a989d031e33154da6e7
魏懿航 <dzzwmqj@outlook.com>
2025-05-12 09:24:43 +0800

    style: 将编译器单独放在一个文件夹里，使得文件目录结构更清晰

--- Compile.bat
+++
@@ -1,7 +0,0 @@
-cas -tRV9E -l main.asm -o .\build\main.o
-@if %ERRORLEVEL% NEQ 0 goto end
-move .\main.lst .\build\main.lst
-cln  .\build\main.o -o .\build\main.bin -m .\build\main.map
-copy .\build\main.lst ..\SIM
-copy .\build\main.bin ..\SIM
-:end
